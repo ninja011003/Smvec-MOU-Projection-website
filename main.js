405fee07613ec3fbbfafdab919e200e387cc33a5
ninja011003 <niranjan011003@gmail.com>
2023-08-31 14:13:31 +0530

    ejs changes

--- main.js
+++ main.js
@@ -41,16 +41,39 @@ app.get('/admin',(req,res)=>{
     
 })
 
-app.post('/upload', upload.single('pdf'), async (req, res) => {
-    try {
-        const response = await uploadPDF(req.file.buffer,req.file.originalname);
-        console.log('File uploaded to Google Drive with status:', response.status);
-        res.send('File uploaded to Google Drive');
-    } catch (err) {
-        console.error('Error uploading to Google Drive:', err);
-        res.status(500).send('Error uploading to Google Drive');
-    }
-});
+// app.post('/upload', upload.single('pdf'), async (req, res) => {
+//     try {
+//         const response = await uploadPDF(req.file.buffer,req.file.originalname);
+//         console.log('File uploaded to Google Drive with status:', response.status);
+//         res.send('File uploaded to Google Drive');
+//     } catch (err) {
+//         console.error('Error uploading to Google Drive:', err);
+//         res.status(500).send('Error uploading to Google Drive');
+//     }
+// });
+
+app.post('/addMou', upload.fields([{ name: 'image' }, { name: 'pdf' }]), (req, res) => {
+    const companyName = req.body.companyName;
+    const yearOfSigning = req.body.yearOfSigning;
+    const duration = req.body.duration;
+    const departments = req.body.dept;
+
+    // Access uploaded files as buffers
+    console.log(req.body.image);
+    const imageBuffer = req.files['image'][0].buffer;
+    const pdfBuffer = req.files['pdf'][0].buffer;
+    console.log('Received Form Data:');
+    console.log('Company Name:', companyName);
+    console.log('Year of Signing:', yearOfSigning);
+    console.log('Duration:', duration);
+    console.log('Selected Departments:', departments);
+
+    console.log('Uploaded Files:');
+    console.log('Image Buffer:', imageBuffer);
+    console.log('PDF Buffer:', pdfBuffer);
+
+
+})
 
 app.get('/links', async (req,res)=>{
     
