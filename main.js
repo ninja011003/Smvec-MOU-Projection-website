ceb2b74488fd2df7e06625a1475f20c2c671e047
ninja011003 <niranjan011003@gmail.com>
2023-09-02 21:20:18 +0530

    upload function corrected

--- main.js
+++ main.js
@@ -42,31 +42,8 @@ app.get('/admin',(req,res)=>{
     
 })
 
-// app.post('/upload', upload.single('pdf'), async (req, res) => {
-//     try {
-//         const response = await uploadPDF(req.file.buffer,req.file.originalname);
-//         console.log('File uploaded to Google Drive with status:', response.status);
-//         res.send('File uploaded to Google Drive');
-//     } catch (err) {
-//         console.error('Error uploading to Google Drive:', err);
-//         res.status(500).send('Error uploading to Google Drive');
-//     }
-// });
 
 app.post('/addMou', upload.fields([{name:'image'},{name:'pdf'}]), async (req, res) => {
-    // const companyName = req.body.companyName;
-    // const yearOfSigning = req.body.yearOfSigning;
-    // const duration = req.body.duration;
-    // const departments = req.body.dept;
-
-    // // Access uploaded files as buffers
-   
-    // console.log('Received Form Data:');
-    // console.log('Company Name:', companyName);
-    // console.log('Year of Signing:', yearOfSigning);
-    // console.log('Duration:', duration);
-    // console.log('Selected Departments:', departments);
-    // console.log(req.files['image'][0].originalname);
     const imgResult = await uploadImage(req.files['image'][0].buffer,req.files['image'][0].originalname);
     const pdfResult = await uploadImage(req.files['pdf'][0].buffer,req.files['pdf'][0].originalname);
     if(imgResult.status==200&&pdfResult.status==200){
@@ -84,10 +61,6 @@ app.post('/addMou', upload.fields([{name:'image'},{name:'pdf'}]), async (req, re
 
 })
 
-// app.get('/links', async (req,res)=>{
-    
-//     res.send(await retrieveLinks());
-// })
 
 
 app.listen(process.env.PORT||3002,()=>{
